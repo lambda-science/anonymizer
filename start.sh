ff4ee7a52c994a71df51c2c2be2e7e9f300cc5ee
Corentin <>
2023-02-20 16:00:15 +0100

    start script

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIR=$(readlink -f $0 | xargs dirname)
+PIDPATH=$DIR/.pid
+
+if [ -f $PIDPATH ]; then
+        kill $(cat $PIDPATH)
+fi
+
+nohup streamlit run Home.py --server.baseUrlPath /Anonymizer --browser.serverAddress lbgi.fr --server.fileWatcherType none --browser.gatherUsageStats false --server.port 8501 --logger.level warning > log 2>&1 &
+echo $! > $DIR/pid
